c459655050324d1b7ed04a3be34fc50e6bc879ae
Pedro Martin del Campo Gonzalez <pedro.martindelcampo.gonzalez@gmail.com>
2025-07-17 06:29:50 -0700

    Center frame on screen. Allow frame resize. Add basic global error handling

--- 01-shapes/src/main/java/com/pedromg/bluej/shapes/App.java
+++ 01-shapes/src/main/java/com/pedromg/bluej/shapes/App.java
@@ -1,10 +1,20 @@
 package com.pedromg.bluej.shapes;
 
+import java.util.logging.Logger;
+
 import com.pedromg.bluej.shapes.ui.MainFrame;
 
 public class App {
+
+    private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+
     public static void main(String[] args) {
-        MainFrame mainFrame = new MainFrame();
-        mainFrame.open();
+        try {
+            MainFrame mainFrame = new MainFrame();
+            mainFrame.open();
+        } catch (Exception e) {
+            LOGGER.severe("An error occurred while starting the application: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
--- 01-shapes/src/main/java/com/pedromg/bluej/shapes/ui/MainFrame.java
+++ 01-shapes/src/main/java/com/pedromg/bluej/shapes/ui/MainFrame.java
@@ -2,12 +2,17 @@ package com.pedromg.bluej.shapes.ui;
 
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
+import javax.swing.plaf.DimensionUIResource;
 
 public class MainFrame {
 
   private static final String TITLE = "Shapes Application";
+
   private static final int WIDTH = 800;
   private static final int HEIGHT = 600;
+
+  private static final int MIN_WIDTH = 400;
+  private static final int MIN_HEIGHT = 300;
 
   private JFrame frame;
 
@@ -15,6 +20,9 @@ public class MainFrame {
     frame = new JFrame(TITLE);
     frame.setSize(WIDTH, HEIGHT);
     frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    frame.setLocationRelativeTo(null); // Center the frame on the screen
+    frame.setResizable(true);
+    frame.setMinimumSize(new DimensionUIResource(MIN_WIDTH, MIN_HEIGHT));
   }
 
   public void open() {
