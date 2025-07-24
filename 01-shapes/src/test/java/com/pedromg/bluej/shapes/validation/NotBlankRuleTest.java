5213ba3a6e6f5ba57870a787f8e746f7dca38a3f
Pedro Martin del Campo Gonzalez <pedro.martindelcampo.gonzalez@gmail.com>
2025-07-24 06:40:51 -0700

    Update NotBlankRuleTest to use MethodSource consistently

--- 01-shapes/src/test/java/com/pedromg/bluej/shapes/validation/NotBlankRuleTest.java
+++ 01-shapes/src/test/java/com/pedromg/bluej/shapes/validation/NotBlankRuleTest.java
@@ -6,20 +6,22 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class NotBlankRuleTest {
 
   @ParameterizedTest
-  @CsvSource({
-      "asdasdasd",
-      "  asd asd asd ",
-      "_________"
-  })
+  @MethodSource("positiveArguments")
   void testValidatePositiveArguments(String text) {
     assertDoesNotThrow(() -> NotBlankRule
         .validate(text, "Oops!"));
+  }
+
+  static Stream<String> positiveArguments() {
+    return Stream.of(
+        "asdasdasd",
+        "  asd asd asd ",
+        "_________");
   }
 
   @ParameterizedTest
