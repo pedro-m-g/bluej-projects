17b545b3c51b3fd631e65def8d5d0e7e6d0a3a99
Pedro Martin del Campo Gonzalez <pedro.martindelcampo.gonzalez@gmail.com>
2025-07-31 21:35:40 -0700

    Add tests for DemoCatalog

--- 01-shapes/src/test/java/com/pedromg/bluej/shapes/demo/DemoCatalogTest.java
+++ 01-shapes/src/test/java/com/pedromg/bluej/shapes/demo/DemoCatalogTest.java
@@ -1,20 +1,26 @@
 package com.pedromg.bluej.shapes.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.pedromg.bluej.shapes.mocks.MockDemo;
 import com.pedromg.bluej.shapes.preconditions.PreConditionsException;
+import java.util.Set;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class DemoCatalogTest {
 
-  // Register with null arguments
   @ParameterizedTest
   @MethodSource("invalidRegisterArgumentsProvider")
   void shouldThrowWhenRegisterNullArguments(String name, Demo demo) {
+    // Given
     DemoCatalog catalog = new DemoCatalog();
+
+    // Then
     assertThrows(PreConditionsException.class, () -> catalog.register(name, demo));
   }
 
@@ -25,10 +31,41 @@ class DemoCatalogTest {
         Arguments.of(null, new CircleDemo()));
   }
 
-  // Find non existent demo
+  @Test
+  void shouldThrowWhenFindingNonExistentDemo() {
+    // Given
+    DemoCatalog catalog = new DemoCatalog();
 
-  // Get available demos
+    // Then
+    assertThrows(PreConditionsException.class, () -> catalog.find("circle"));
+  }
 
-  // Find demo correctly
+  @Test
+  void shouldRetrieveAvailableDemos() {
+    // Given
+    DemoCatalog catalog =
+        new DemoCatalog()
+            .register("circle", new MockDemo())
+            .register("square", new MockDemo())
+            .register("triangle", new MockDemo());
 
+    // When
+    Set<String> actualDemos = catalog.availableDemos();
+
+    // Then
+    assertEquals(Set.of("circle", "square", "triangle"), actualDemos);
+  }
+
+  @Test
+  void shouldFindExistingCommandSuccessfully() {
+    // Given
+    Demo expectedDemo = new MockDemo();
+    DemoCatalog catalog = new DemoCatalog().register("circle", expectedDemo);
+
+    // When
+    Demo actualDemo = catalog.find("circle");
+
+    // Then
+    assertEquals(expectedDemo, actualDemo);
+  }
 }
